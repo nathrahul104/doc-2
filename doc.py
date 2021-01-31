40025be170b038ce9e69da00fe2171be3494c5c8
Rahul Nath <64326643+rahulnath2904@users.noreply.github.com>
2021-01-31 23:46:26 +0530

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,7 @@
+mlist=[25,22,15,25,25,22,22,22]
+feq={}
+for items in mlist:
+    if items not in feq:
+        feq[items]=mlist.count(items)
+for key,values in feq.items():
+    print(key," ",values)
